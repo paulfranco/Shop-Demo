0fe441eab9c6011c601eac59e7d465cd6930bf5b
Paul Franco <mail@paulfran.co>
2020-08-29 00:17:44 -0700

    updated featured product model

---
+++ Shop Demo/Model/Product.swift
@@ -0,0 +1,22 @@
+//
+//  Product.swift
+//  Shop Demo
+//
+//  Created by Paul Franco on 8/28/20.
+//
+
+import Foundation
+
+struct Product {
+    private(set) public var productTitle: String
+    private(set) public var productImage: String
+    private(set) public var productPrice: Double
+    private(set) public var productDescription: String!
+    
+    init(productTitle: String, productImage: String, productPrice: Double, productDescription: String) {
+        self.productTitle = productTitle
+        self.productImage = productImage
+        self.productPrice = productPrice
+        self.productDescription = productDescription
+    }
+}
