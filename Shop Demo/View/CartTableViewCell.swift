11d8b55b42394e24f517e58aa416a19364f34999
Paul Franco <mail@paulfran.co>
2020-08-29 04:50:36 -0700

    CartVC and CartTableViewCell

---
+++ Shop Demo/View/CartTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CartTableViewCell.swift
+//  Shop Demo
+//
+//  Created by Paul Franco on 8/29/20.
+//
+
+import UIKit
+
+class CartTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
