bd66a7fe12a658f5a9f2162725701471a9b8326b
Paul Franco <mail@paulfran.co>
2020-08-28 22:42:00 -0700

    added collection cell, and set up Ui for the collection cell

---
+++ Shop Demo/View/FeaturedCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeaturedCell.swift
+//  Shop Demo
+//
+//  Created by Paul Franco on 8/28/20.
+//
+
+import UIKit
+
+class FeaturedCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
